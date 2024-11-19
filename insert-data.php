4c7f97aeac4b10a15358496a2b5561ec601c5918
Julian Canepa <3414201+jcanepa@users.noreply.github.com>
2024-11-19 14:10:46 -0700

    Fix typo, make comments more clear in insert-data.php

--- insert-data.php
+++ insert-data.php
@@ -2,9 +2,9 @@
 
 /**
  * Validate the request by conditionally redirecting the user.
- * @TODO: Replace all `true` values with your logic on lines 10-12
+ * @TODO: Replace all `true` values on lines 10-12 with your logic
  *      Line 10: the http request type is POST
- *      Line 11: the submit button doesn't match the value in index.html
+ *      Line 11: the submit button matches the value in index.html
  *      Line 12: $_POST['tv-show'] contains a meaningful value (not null, is alphanumeric, no HTML/script/SQL)
  */
 $is_post_request = true;
@@ -19,16 +19,17 @@ if (!$valid) {
 
 /**
  * Save validated $_POST['tv-show'] value to the database.
- * Table schema for reference: create table `tv_shows` (`name` VARCHAR(20) primary key unique not null, `count` int not null default 0);
+ * Table schema for reference: 
+ * create table `tv_shows` (`name` VARCHAR(20) primary key unique not null, `count` int not null default 0);
  * @TODO: Insert validated submission data into the `tv_shows` table on line 26
- *      Optionally use a try catch block, and display error messages to the user.
+ *      Optionally use a try catch block, and display error messages to the user when an exception occurs.
  *      Optionally increment the show `count` if the name is already stored.
  */
 $pdo = new \PDO('sqlite:tv_shows.db');
 
 /**
  * Display stored data, four entries were inititally provided.
- * No need to modify.
+ * No need to modify this.
  */
 foreach ($pdo->query("select `name` from tv_shows") as $row) {
     echo '<li>' . $row['name'] . '</li>';
